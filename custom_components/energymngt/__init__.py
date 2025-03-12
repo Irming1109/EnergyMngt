2676abacc2a6557f17eda071760b3fb4292b6ce1
Kasper Holm <repzak@gmail.com>
2025-03-12 12:22:02 +0100

    w

--- custom_components/energymngt/__init__.py
+++ custom_components/energymngt/__init__.py
@@ -1,32 +1,29 @@
-from homeassistant.core import HomeAssistant
-
-DOMAIN = "energymngt"
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up integrationen fra konfigurationsfilen."""
-    hass.states.async_set(f"{DOMAIN}.status", "running")
-    return True
-    
 import logging
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import discovery
+from homeassistant.loader import async_get_integration
 
-from .const import DOMAIN
+from .const import DOMAIN, PLATFORMS, STARTUP
 
 # Initialiserer et logger for integrationen
-_LOGGER = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 # Denne funktion bliver kaldt, når integrationen er blevet konfigureret gennem UI (via Config Flow)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Gør integrationen klar til brug baseret på de indstillinger brugeren har givet."""
-    _LOGGER.info(f"Konfiguration af enermymngt entry: {entry.entry_id}")
+
+    hass.data.setdefault(DOMAIN, {})
+    integration = await async_get_integration(hass, DOMAIN)
+    LOGGER.info(STARTUP, integration.version)
+
+    LOGGER.info(f"Konfiguration af enermymngt entry: {entry.entry_id}")
     
     # Her kan du initialisere sensorer eller andre enheder, hvis nødvendigt
     # For eksempel kan du tilføje sensorer som en del af entry konfigurationen
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
     await async_setup_entry(hass, entry, async_add_entities)
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     # Returner True for at indikere at opsætningen er gennemført
     return True
@@ -34,5 +31,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 # Rydder op, når integrationen fjernes
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Fjern eventuelle ressourcer, når entry fjernes."""
-    _LOGGER.info(f"Fjerner entry: {entry.entry_id}")
+    LOGGER.info(f"Fjerner entry: {entry.entry_id}")
+    return True
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up integrationen fra konfigurationsfilen."""
+    hass.states.async_set(f"{DOMAIN}.status", "running5")
     return True
--- custom_components/energymngt/const.py
+++ custom_components/energymngt/const.py
@@ -1,2 +1,17 @@
 """Const used in the integration."""
+
+# Startup banner
+STARTUP = """
+-------------------------------------------------------------------
+Strømligning
+
+Version: %s
+This is a custom integration
+If you have any issues with this you need to open an issue here:
+https://github.com/mtrab/stromligning/issues
+-------------------------------------------------------------------
+"""
+
 DOMAIN = "energymngt"
+
+PLATFORMS = ["sensor", "binary_sensor"]

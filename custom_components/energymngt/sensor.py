210c554cc00e289d7d65d434762581c3f1ac1f9f
Irming1109 <77323794+Irming1109@users.noreply.github.com>
2025-03-12 10:59:29 +0100

    Update sensor.py

--- custom_components/energymngt/sensor.py
+++ custom_components/energymngt/sensor.py
@@ -2,20 +2,20 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.entity import EntityDescription
 import random
 
-DOMAIN = "energymngt"
+from .const import DOMAIN
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Opsætning af sensoren."""
-    async_add_entities([RandomNumberSensor()], True)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    """Opsæt sensoren baseret på UI-konfigurationen."""
+    sensor_name = entry.data.get("sensor_name", "Min Sensor")  # Henter sensor-navnet fra entry
+    async_add_entities([RandomNumberSensor(sensor_name)], True)
 
 class RandomNumberSensor(SensorEntity):
     """En simpel sensor, der viser et tilfældigt tal."""
 
-    def __init__(self):
+    def __init__(self, name):
         """Initialiser sensoren."""
-        self._attr_name = "Tilfældig Sensor"
-        self._attr_unique_id = "random_sensor_1"
-        self._attr_native_unit_of_measurement = "værdi"
+        self._attr_name = name
+        self._attr_unique_id = f"sensor_{name.lower().replace(' ', '_')}"
         self._state = None
 
     def update(self):

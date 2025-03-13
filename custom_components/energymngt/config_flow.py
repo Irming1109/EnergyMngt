f961dc7a67f154c857ab1fa203e7df8d664e558b
Kasper Holm <repzak@gmail.com>
2025-03-13 22:57:42 +0100

    fix3

--- custom_components/energymngt/config_flow.py
+++ custom_components/energymngt/config_flow.py
@@ -14,8 +14,13 @@ class EnergymngtConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             # Create a new entry with what, user has entered
-            return self.async_create_entry(title="Energi Management Integration", data=user_input)
-            
+                       
+            return self.async_create_entry(
+                title=user_input[CONF_NAME],
+                data={"name": user_input[CONF_NAME]},
+                options=user_input,
+                description=f"Strømligning - {user_input[CONF_NAME]}",
+            )
             
         # Defines needed inputs from user (example: sensor_name)
         schema = vol.Schema(

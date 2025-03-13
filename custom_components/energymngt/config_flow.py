22c1fb92c08c3a0646434ae492e2ef2ee1c2c7f5
Kasper Holm <repzak@gmail.com>
2025-03-13 23:03:36 +0100

    fix 4

--- custom_components/energymngt/config_flow.py
+++ custom_components/energymngt/config_flow.py
@@ -3,6 +3,41 @@ import voluptuous as vol
 
 from .const import DOMAIN
 
+class EnergymngtOptionsFlow(config_entries.OptionsFlow):
+    """EnergyMngt options flow handler."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize EnergyMngt options flow."""
+        self.config_entry = config_entry
+        self._errors = {}
+
+    async def _do_update(
+        self, *args, **kwargs  # pylint: disable=unused-argument
+    ) -> None:
+        """Update after settings change."""
+        await async_unload_entry(self.hass, self.config_entry)
+        await async_setup_entry(self.hass, self.config_entry)
+
+    async def async_step_init(self, user_input: Any | None = None):
+        """Handle the initial options flow step."""
+        errors = {}
+
+        api = self.hass.data[DOMAIN][self.config_entry.entry_id]._data
+
+        if user_input is not None and "base" not in errors:
+            LOGGER.debug("Saving settings")
+           
+            async_call_later(self.hass, 2, self._do_update)
+            return self.async_create_entry(
+                title=self.config_entry.data.get(CONF_NAME),
+                data=user_input,
+                description=f"Energy Management - {self.config_entry.data.get(CONF_NAME)}",
+            )
+
+        LOGGER.debug("Showing options form")
+
+        return self.async_show_form(step_id="init", data_schema=scheme, errors=errors)
+
 class EnergymngtConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1

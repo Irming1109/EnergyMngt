2539b72a1578bc818279723ec129892e24fec702
Kasper Holm <repzak@gmail.com>
2025-03-16 12:40:07 +0100

    Return a dictionary

--- custom_components/energymngt/services.py
+++ custom_components/energymngt/services.py
@@ -23,8 +23,8 @@ async def async_setup_services(hass: HomeAssistant) -> None:
         #sc = call.data
         #LOGGER.debug("called yearly with %r", sc)
 
-        currency = call.data.get("currency", "N/A")
-        hass.states.set("energymngt.hello_world2", "Hello World 2")
+        currency = "ddd"; #call.data.get("currency", "N/A")
+        #hass.states.set("energymngt.hello_world2", "Hello World 2")
         return {"currency": currency, "message": "Hello World 2"}  # Return a dictionary
 
         # For testing purposes, just return a static value
